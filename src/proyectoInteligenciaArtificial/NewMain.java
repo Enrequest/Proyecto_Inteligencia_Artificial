bed9557416e23cbfbf42759a8a1d72e48be44c07
Enrequest <enrique_silvestre28@outlook.com>
2019-04-15 18:51:27 -0400

    Modificicacion de NewMain

--- src/proyectoInteligenciaArtificial/NewMain.java
+++ src/proyectoInteligenciaArtificial/NewMain.java
@@ -16,6 +16,7 @@ public class NewMain {
     public static void main(String[] args)throws Exception {
         Scanner sc = new Scanner(new FileReader("DatosMapa.txt"));
         int n = 63;
+        String mapa[][] = new String[n][n];
         Grafo g = new Grafo(n);
         System.out.println("Ingresando vertices, para cancelar escriba -1 -1");
         while(true){
@@ -25,6 +26,10 @@ public class NewMain {
                 System.out.println("Insercion de vertices terminado");
                 break;
             }
+            sc.nextLine();
+            String c = sc.nextLine();
+            mapa[a][b] = c;
+            mapa[b][a] = c;
             g.agregarArista(a, b);
         }
         System.out.println("Ingrese nodo s y t");
@@ -37,5 +42,14 @@ public class NewMain {
             System.out.printf("%d ",act);
         }
         System.out.println();
+
+        Set<String> set = new LinkedHashSet<String>(); 
+        int i = path.removeFirst();
+        int j = path.removeFirst();
+        for(int act: path){
+            set.add(mapa[i][j]);
+            i = j;
+            j = act;
+        }
     }
 }
